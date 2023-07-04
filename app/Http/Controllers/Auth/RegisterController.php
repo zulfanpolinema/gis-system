1a4143d6497f99c8a838d8f3372ab4373daf855e
zulfanpolinema <1941720152@student.polinema.ac.id>
2023-07-04 17:15:59 +0700

    Menambahkan pilihan role pada register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'role' => ['required'],
         ]);
     }
 
@@ -64,10 +65,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ])->assignRole('Pengepul');
+        ]);
+
+        if ($data['role'] == 'pengepul') {
+            $user->assignRole('Pengepul');
+        } else if ($data['role'] == 'petani') {
+            $user->assignRole('Petani');
+        }
+
+        return $user;
     }
 }
